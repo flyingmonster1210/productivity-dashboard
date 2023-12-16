75c133b8ad59ec13e843d210042098cedbfa8469
Wayne <zweijie1210@outlook.com>
2023-12-15 17:13:36 -0800

    Initilize Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,81 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Checkbox, Input, Divider } from 'antd'
 
-function Login() {
-  return <div>Login</div>
+const onFinish = (values) => {
+  console.log('Success:', values)
+}
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo)
+}
+
+function Login () {
+  return (
+    <div className='login_body_wrap'>
+      <div className='form_icon_wrap'>
+        <div className="icon"></div>
+        <h2>Dashboard</h2>
+      </div>
+      <Form
+        className='form_wrap'
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <p className='form_title'>Please Login</p>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 9,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+
+        <Divider />
+        <Link to='/register' style={{ margin: 'auto' }}>Create New Account</Link>
+      </Form>
+    </div>
+  )
 }
 
 export default Login
