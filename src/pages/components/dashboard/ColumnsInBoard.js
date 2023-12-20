2536e536f2b8c0f5a15deaff4a6085ca34faeb9e
Wayne <zweijie1210@outlook.com>
2023-12-20 00:55:44 -0800

    Fix bug - rearrange dataList

--- src/pages/components/dashboard/ColumnsInBoard.js
+++ src/pages/components/dashboard/ColumnsInBoard.js
@@ -9,27 +9,27 @@ const dataList = [
     id: '04123aa',
     columnName: 'To do',
     tasks: [
-      { id: '1234d0', name: 'To do0', content: 'To do0' },
-      { id: 'as2331', name: 'To do1', content: 'To do1' },
-      { id: 'ad3322', name: 'To do2', content: 'To do2' },
+      { id: '1234d0', name: '1', content: 'To do0' },
+      { id: 'as2331', name: '2', content: 'To do1' },
+      { id: 'ad3322', name: '3', content: 'To do2' },
     ],
   },
   {
     id: '19037841',
     columnName: 'Doing',
     tasks: [
-      { id: '0393ba0', name: 'Doing0', content: 'Doing0' },
-      { id: 'a2220b1', name: 'Doing1', content: 'Doing1' },
-      { id: '2ab3294', name: 'Doing2', content: 'Doing2' },
+      { id: '0393ba0', name: '4', content: 'Doing0' },
+      { id: 'a2220b1', name: '5', content: 'Doing1' },
+      { id: '2ab3294', name: '6', content: 'Doing2' },
     ],
   },
   {
     id: '2000927',
     columnName: 'Done',
     tasks: [
-      { id: '09384ab7', name: 'Done0', content: 'Done0' },
-      { id: '1739347a', name: 'Done1', content: 'Done1' },
-      { id: '2ab67393', name: 'Done2', content: 'Done2' },
+      { id: '09384ab7', name: '7', content: 'Done0' },
+      { id: '1739347a', name: '8', content: 'Done1' },
+      { id: '2ab67393', name: '9', content: 'Done2' },
     ],
   },
 ]
@@ -47,14 +47,21 @@ const changeItem = (src, des, data, setData) => {
   const srcColId = Number(srcId)
 
   const newData = [...data]
-  newData[desColId].tasks.splice(desItemIndex, 0, newData[srcColId].tasks[srcItemIndex])
-  newData[srcColId].tasks.splice(srcItemIndex, 1)
+  if (srcColId !== desColId) {
+    newData[desColId].tasks.splice(desItemIndex, 0, newData[srcColId].tasks[srcItemIndex])
+    newData[srcColId].tasks.splice(srcItemIndex, 1)
+  }
+  else {
+    const newTasks = newData[srcColId].tasks.filter((_, index) => index !== srcItemIndex)
+    newTasks.splice(desItemIndex, 0, data[srcColId].tasks[srcItemIndex])
+    newData[srcColId].tasks = newTasks
+  }
   setData(newData)
   // console.log(newData)
 }
 
 const handleDragEnd = (result, data, setData) => {
-  console.log('result:', result)
+  // console.log('result:', [result.source.index, result.destination.index])
 
   const { source: src, destination: des, type } = result
   if (!des || !src) return
